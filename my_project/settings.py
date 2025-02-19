961028dbf0af2ed76fc1b92f50d5acd8ba9f5953
SonyaJane <sonya.ridden@gmail.com>
2025-02-19 14:38:41 +0000

    Added folers and settings for Heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z#7%le21el55u82d6fmrg!hs9hrx48g%idu3dv5k$7m!q#+xy#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
